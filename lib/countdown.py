c5cb0d3553f9b2f462352e603669878ae66858cf
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-13 14:55:36 -0500

    there is a modual import  error

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,6 @@
 # your code goes here!
+def countdown_with_sleep(number):
+    while(number > 0):
+        print(f'{number} SECOND(S)!')
+        number = number - 1
+    return print( "HAPPY NEW YEAR!")
